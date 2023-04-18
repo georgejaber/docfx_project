c39128a86ca4367bc7a50ebafb4e4b7c2c04c9b4
George Jaber <67227922+georgejaber@users.noreply.github.com>
2023-04-18 18:21:00 -0500

    Add files via upload

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -3,21 +3,21 @@
 
 
 echo "loging into azure...."
-az login --service-principal -u %AZUREAPPID% -p %AZUREAPPKEY% --tenant %AZUREAPPTENANT%
+az login --service-principal -u $AZUREAPPID -p $AZUREAPPKEY --tenant $AZUREAPPTENANT
 
-mkdir %BUILD_SOURCEDIRECTORY%/_dl
-mkdir %BUILD_SOURCEDIRECTORY%/_bin
-mkdir %BUILD_SOURCEDIRECTORY%/_publish
+mkdir $BUILD_SOURCEDIRECTORY/_dl
+mkdir $BUILD_SOURCEDIRECTORY/_bin
+mkdir $BUILD_SOURCEDIRECTORY/_publish
 
-curl -O %BUILD_SOURCEDIRECTORY%/_dl/docfx.zip "https://github.com/dotnet/docfx/releases/download/v2.65.3/docfx-win-x64-v2.65.3.zip"
+curl -O $BUILD_SOURCEDIRECTORY/_dl/docfx.zip "https://github.com/dotnet/docfx/releases/download/v2.65.3/docfx-win-x64-v2.65.3.zip"
 
-Expand-Archive -Force %BUILD_SOURCEDIRECTORY%/_dl/docfx.zip %BUILD_SOURCEDIRECTORY%/_publish/
+Expand-Archive -Force $BUILD_SOURCEDIRECTORY/_dl/docfx.zip $BUILD_SOURCEDIRECTORY/_publish/
 
-cd %BUILD_SOURCEDIRECTORY%/%DOCSFOLDER%
+cd $BUILD_SOURCEDIRECTORY/$DOCSFOLDER
 
-start /d %BUILD_SOURCEDIRECTORY%/_bin/docfx/ docfx.exe
+start /d $BUILD_SOURCEDIRECTORY/_bin/docfx/ docfx.exe
 
 
-move /y %BUILD_SOURCEDIRECTORY%/%DOCSFOLDER%/_site/*.* %BUILD_SOURCEDIRECTORY%/_publish/
+move /y $BUILD_SOURCEDIRECTORY/$DOCSFOLDER/_site/*.* $BUILD_SOURCEDIRECTORY/_publish/
 
-az storage blob upload-batch -s %BUILD_SOURCEDIRECTORY%/_publish/ -d \$web --acount-name $AZURESTORAGE
+az storage blob upload-batch -s $BUILD_SOURCEDIRECTORY/_publish/ -d \$web --acount-name $AZURESTORAGE
