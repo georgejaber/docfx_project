e6cfefb83236c2aaf4b003a94776f2cad856f67a
george <jaber.george2@gmail.com>
2023-05-20 10:02:16 +0300

    light

--- templates/material/styles/main.js
+++ templates/material/styles/main.js
@@ -1,4 +1,1 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
-export default {
-    defaultTheme: 'light'
-}
